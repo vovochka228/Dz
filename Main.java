bdd340129967d1cdd18d7601e3058d281820e9a7
vovochka228 <49089327+vovochka228@users.noreply.github.com>
2019-04-06 12:20:36 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.chart.*;
+import javafx.scene.Group;
+ 
+public class Main extends Application {
+ 
+    @Override public void start(Stage stage) {
+        Scene scene = new Scene(new Group());
+        stage.setTitle("Imported Fruits");
+        stage.setWidth(500);
+        stage.setHeight(500);
+ 
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("China", 1275),
+                new PieChart.Data("India", 1017),
+                new PieChart.Data("Brazil", 172),
+                new PieChart.Data("UK", 59),
+                new PieChart.Data("USA", 285));
+        final PieChart chart = new PieChart(pieChartData);
+        chart.setTitle("Imported Fruits");
+
+        ((Group) scene.getRoot()).getChildren().add(chart);
+        stage.setScene(scene);
+        stage.show();
+    }
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
